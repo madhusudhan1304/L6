ce279c164e1808730617867fd32409ecb7129330
madhusudhan1304 <madhusudhanr1304@gmail.com>
2023-01-06 23:53:41 +0530

    second

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -10,7 +10,7 @@ const extractCSRFToken = (html) => {
   return $("[name=_csrf]").val();
 };
 
-describe("Todo Application", function () {
+describe("Todo App", function () {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     server = app.listen(5000, () => {});
@@ -31,7 +31,7 @@ describe("Todo Application", function () {
     const csrfToken = extractCSRFToken(text);
 
     const response = await agent.post("/todos").send({
-      title: "Buy milk",
+      title: "Buy book",
       dueDate: new Date().toISOString(),
       _csrf: csrfToken,
     });
@@ -42,7 +42,7 @@ describe("Todo Application", function () {
     let res = await agent.get("/");
     let csrfToken = extractCSRFToken(res.text);
     await agent.post("/todos").send({
-      title: "Wash Dishes",
+      title: "do homework",
       dueDate: new Date().toISOString(),
       _csrf: csrfToken,
     });
@@ -70,7 +70,7 @@ describe("Todo Application", function () {
     let csrfToken = extractCSRFToken(res.text);
 
     await agent.post("/todos").send({
-      title: "Complete levels",
+      title: "Complete  all levels",
       dueDate: new Date().toISOString(),
       _csrf: csrfToken,
     });
