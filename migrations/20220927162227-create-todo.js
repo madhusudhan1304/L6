50aa1ec0f1045120f50c6d5bb020ce765da674c0
madhusudhan1304 <madhusudhanr1304@gmail.com>
2023-01-08 18:59:20 +0530

    Revert "third"
    This reverts commit fd974f1ed21e4bab165f396a508ab67bc2444500.

--- migrations/20220927162227-create-todo.js
+++ migrations/20220927162227-create-todo.js
@@ -2,7 +2,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-<<<<<<< HEAD
     await queryInterface.createTable("Todos", {
       id: {
         allowNull: false,
@@ -28,51 +27,8 @@ module.exports = {
         type: Sequelize.DATE,
       },
     });
-=======
-    await queryInterface.createTable(
-      "Todos",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        title: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          validate: {
-            notNull: true,
-            len: 5,
-          },
-        },
-        dueDate: {
-          type: Sequelize.DATEONLY,
-        },
-        completed: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      },
-      {
-        tableName: "todo",
-      }
-    );
->>>>>>> 6724317 (third)
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Todos");
   },
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 6724317 (third)
